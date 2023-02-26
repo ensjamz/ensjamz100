64b77ffe9d33cfcf6273d478a74c4ee11d79f6d3
ensjamz <126366385+ensjamz@users.noreply.github.com>
2023-02-25 18:01:43 -0800

    Update emoji-pop.js

--- emoji-pop.js
+++ emoji-pop.js
@@ -5,7 +5,7 @@ class Fountain {
     this.autoAddParticle = false;
     this.height = document.documentElement.clientHeight;
     this.sizes = [15, 20, 25, 35, 45];
-    this.variants = ["☠️", "☠️"];
+    this.variants = ["💯", "💯"];
     this.addHandlers();
     this.loop();
   }
@@ -13,9 +13,9 @@ class Fountain {
   loop() {
     if (this.autoAddParticle && this.particles.length < this.limit) {
       // if (document.getElementById("field-email") === document.activeElement) {
-      //   this.variants = ["☠️"];
+      //   this.variants = ["💯"];
       // } else {
-      //   this.variants = ["☠️"];
+      //   this.variants = ["💯"];
       // }
       this.createParticle();
     }
